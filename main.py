2f130942e73dd978049e5ec3d3c1b3ab2e1910cf
sohailahmedjokhio <139850469+sohailahmedjokhio@users.noreply.github.com>
2024-01-27 02:54:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to the Tip Calculator")
+
+bill = float(input("What was the total bill?\n$"))
+percentage = float(input("What percetntage tip you want to give? 10, 12, 15\n")) 
+people = int(input("How many people to split the bill?\n"))
+net_percentage = float(percentage / 100)
+tip = (bill / people) * net_percentage
+payable_bill = bill / people + tip
+total_bill = format(payable_bill, '.2f')
+print(f"Each Person Should pay: {total_bill}")
