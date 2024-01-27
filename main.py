882b733426a075eb6de5496363c94a5b6986ae8a
sohailahmedjokhio <139850469+sohailahmedjokhio@users.noreply.github.com>
2024-01-27 03:14:43 +0000

    Updated Code to show tip amount too.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ percentage = float(input("What percetntage tip you want to give? 10, 12, 15\n"))
 people = int(input("How many people to split the bill?\n"))
 net_percentage = float(percentage / 100)
 tip = (bill / people) * net_percentage
+round_tip = format(tip, '.2f')
 payable_bill = bill / people + tip
 total_bill = format(payable_bill, '.2f')
-print(f"Each Person Should pay: {total_bill}")
+print(f"Each Person Should pay: {total_bill}$, Tip will be: {round_tip}$ for each person.")
